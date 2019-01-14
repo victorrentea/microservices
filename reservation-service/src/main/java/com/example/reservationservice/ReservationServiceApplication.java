a34acf36b7a24cc4a1007124f2ee71fca48bba5a
Victor Rentea <victorrentea@gmail.com>
2019-01-14 13:27:58 +0200

    first deployable microservice

---
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,78 @@
+package com.example.reservationservice;
+
+import java.util.stream.Stream;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+
+}
+
+//class DummyCLR implements CommandLineRunner {
+//	@Autowired
+//	private ReservationRestRepository repo;
+//
+//	public void run(String... args) throws Exception {
+//		Stream.of("Bianca", "Marian", "Victor", "Adrian", "Eugen")
+//			.map(mapper)
+//	}
+//	
+//}
+
+
+@RepositoryRestResource(path= "reservations")
+interface ReservationRestRepository extends JpaRepository<Reservation, Long> {
+	
+}
+
+
+@Entity
+class Reservation {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String reservationName;
+	
+	private Reservation() {
+	}
+	
+	public Reservation(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+	
+	
+}
+
