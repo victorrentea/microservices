7476966a69a8e77cbae9786925b99e112f240003
Victor Rentea <victorrentea@gmail.com>
2019-01-14 17:05:23 +0200

    tot ce a ramas

--- reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
@@ -1,28 +1,54 @@
 package com.example.reservationclient;
 
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+// daca vrei vreodata mai multe cozi de iesire, le poti definit pe toate 
+// intr-o interfgata pe care ti-o creezi tu "cu manuntele astea doua" - Ciorbea
+//interface MyOutputQueues {
+//	@Output("createCustomer")
+//	MessageChannel createCustomer();
+//	@Output("activateCustomer")
+//	MessageChannel activateCustomer();
+//}
+
+@EnableHystrix
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class ReservationClientApplication {
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
@@ -46,6 +72,11 @@ class MyController {
 	@Autowired
 	private RestTemplate rest;
 	
+	public List<String> fallbackResponse() {
+		return asList("Basescu");
+	}
+	
+	@HystrixCommand(fallbackMethod = "fallbackResponse")
 	@GetMapping("reservations")
 	public List<String> getReservationNames() {
 		List<String> lista14 = new ArrayList<String>(); // NU ai cum sa obtii <String> din lista14 datorita Type Erasure
@@ -57,13 +88,26 @@ class MyController {
 		ParameterizedTypeReference<Resources<Reservation>> type = 
 				new ParameterizedTypeReference<Resources<Reservation>>() { };
 		
-		ResponseEntity<Resources<Reservation>> entity = rest.exchange("http://localhost:9080/reservations", HttpMethod.GET, null, type);
+		ResponseEntity<Resources<Reservation>> entity = 
+				rest.exchange("http://reservation-service/reservations", 
+						HttpMethod.GET, null, type);
 		
 		return entity.getBody()
 			.getContent()
 			.stream()
 			.map(Reservation::getReservationName)
 			.collect(toList());
+	}
+	
+	@Autowired
+	private Source source;
+	private final static Logger log = LoggerFactory.getLogger(MyController.class);
+	
+	@PostMapping("reservation")
+	public void createReservation(@RequestBody Reservation reservation) {
+		source.output().send(MessageBuilder.withPayload(reservation.getReservationName()).build());
+		log.debug("Message Sent");
+		System.out.println("Message Sent");
 	}
 }
 
--- reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -6,25 +6,51 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.esotericsoftware.minlog.Log;
+
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableBinding(Sink.class)
+@IntegrationComponentScan
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
 	}
+}
 
+@MessageEndpoint
+class ReservationCreator {
+	private final static Logger log = LoggerFactory.getLogger(ReservationCreator.class);
+	@Autowired
+	private ReservationRestRepository repo;
+	
+	@ServiceActivator(inputChannel = Sink.INPUT)
+	public void createReservation(String rn) {
+		System.out.println("Primit rezervare de creat: " + rn);
+		log.debug("execut");
+		repo.save(new Reservation(rn));
+	}
 }
 
 @Component
