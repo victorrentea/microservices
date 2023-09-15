ab42f002600eed67980497559112bee0b06285ab
Victor Rentea <victorrentea@gmail.com>
2023-09-15 09:19:42 +0300

    add actuator cascading

---
+++ reservation-client/src/main/java/victor/ms/client/ReservationServiceHealthMetric.java
@@ -0,0 +1,32 @@
+package victor.ms.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class ReservationServiceHealthMetric implements HealthIndicator {
+  @Autowired
+  private RestTemplate rest;
+
+  @Override
+  public Health health() {
+    try {
+      String url = "http://reservation-service/actuator/health";
+      Map<String, Object> responseMap = rest.getForObject(url, Map.class);
+      if (responseMap.get("status").equals("UP")) {
+        return Health.up().build();
+      }
+    } catch (RestClientException e) {
+      log.error("Health check failed :  " + e);
+    }
+    return Health.down().build();
+  }
+}
