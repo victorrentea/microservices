2e43dd329282eae8bb9886d4d64549307c5688c9
Victor Rentea <victorrentea@gmail.com>
2024-06-05 09:29:19 +0300

    pt gicu

--- notification/src/main/java/victor/training/ms/notification/HelloRest.java
+++ notification/src/main/java/victor/training/ms/notification/HelloRest.java
@@ -2,11 +2,21 @@ package victor.training.ms.notification;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.jcajce.provider.symmetric.AES.CFB;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import victor.training.ms.notification.CustomerClient.CustomerDto;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.Collections.synchronizedMap;
 
 @Slf4j
 @RestController
@@ -34,4 +44,26 @@ public class HelloRest {
 //    var dto = customerClient.getCustomer("margareta");
     return "Hello from Notification: " + dto;
   }
+
+  // asta-i pemntru Gicu din spate
+//  @GetMapping("product/{id}")
+//  public CompletableFuture<String> getProduct(long id) {
+//    CompletableFuture<String> cf = new CompletableFuture<>();
+//    ongoingRequests.put(id, cf);
+//    return cf; // ii spune lui SPring sa lase in asteptare req HTTP dar fara sa blocheze threadul curent
+//  }
+//  Map<Long, CompletableFuture<String>> ongoingRequests = synchronizedMap(new HashMap<>());
+//  @Scheduled(fixedDelay = 1000)
+//  public void method() {
+//    Map<Long, CompletableFuture<String>> clone;
+//    synchronized (ongoingRequests) {
+//      clone = new HashMap<>(ongoingRequests);
+//      ongoingRequests.clear();
+//    }
+//    Set<Long> productIds = clone.keySet();
+//    Map<Long, String> results = restApi.getAll(productIds);
+//    results.forEach((productId, productStr) -> clone.get(productId)
+//        .complete(results.get(productId))); // aici un request HTTP primeste raspunsul asteptat,
+//    // apoi spring inchide conex http cu lientu ramasa pana acum deschisa
+//  }
 }
