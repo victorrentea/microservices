74d6a267026365b7826a41f319db3fadc9a738a6
Victor Rentea <victorrentea@gmail.com>
2025-02-10 09:29:46 +0200

    feign + eureka

--- order/src/main/java/victor/training/ms/order/OrderApp.java
+++ order/src/main/java/victor/training/ms/order/OrderApp.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
@@ -23,7 +24,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @EnableFeignClients
-//@EnableDiscoveryClient // needed if using RestTemplate
+@EnableDiscoveryClient // needed if using RestTemplate
 @RequiredArgsConstructor
 public class OrderApp {
   private final OrderRepo orderRepo;
